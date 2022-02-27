c10e6d4935d471812ca9ae5c5ed20ffe6b1c7c14
nurulamin092 <mnurulamin092@gmial.com>
2022-02-27 10:03:59 +0600

    dog  show display

--- js/app.js
+++ js/app.js
@@ -7,10 +7,21 @@ const loadTheDog = () => {
         .then(res => res.json())
         .then(data => displayTheDog(data));
 }
-
 const displayTheDog = dogs => {
+    const dogsInput = document.getElementById('dogs');
     dogs.forEach(dog => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+        <img src="${dog.image.url}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${dog.name}</h5>
+            <p class="card-text">${dog.temperament}</p>
+        </div>
+        `;
+        dogsInput.appendChild(div);
         console.log(dog);
     });
 }
-loadTheDog()
+//loadTheDog()
